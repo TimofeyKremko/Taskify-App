c41b5243729b5ddbbde75e58ca490b92f9288a8c
Timofey Kremko <timkremko@gmail.com>
2022-10-22 12:44:55 +0200

    final project

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,21 +1,36 @@
-import React from 'react'
-import './styles.css'
+import React, { useRef } from "react";
+import "./styles.css";
 
-interface Props{
-  todo:string;
+interface props {
+  todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
-  handleAdd:(e: React.FormEvent) => void;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className='input' onSubmit={handleAdd}>
-      <input type="input" placeholder='Enter a task' className='input__box'
-      value={todo}
-      onChange={(e) => setTodo(e.target.value)}/>
-      <button className='input__submit' type='submit'>Go</button>
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a Task"
+        value={todo}
+        ref={inputRef}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
+      <button type="submit" className="input_submit">
+        GO
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
